117d71c3678f6271030db724ad55e725887be00f
Veeramuhilan K <veeramuhilan.karthikeyan@kulu.com>
2026-08-05 20:10:38 +0530

    Session_4  Task_4

---
+++ src/test/Session_3/task_3.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('Navigate to saucedemo and check title and URL', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page).toHaveTitle('Swag Labs');
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+});
